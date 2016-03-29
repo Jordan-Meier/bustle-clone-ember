f71bccaf0ce64c0f0b670feb3bedc8b4d92df93a
Jordan Meier <jbirdmeier@gmail.com>
2016-03-29 09:42:58 -0700

    change the order of model properties

--- app/models/story.js
+++ app/models/story.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  author: DS.attr(),
-  date: DS.attr(),
   headline: DS.attr(),
+  date: DS.attr(),
+  author: DS.attr(),
   story: DS.attr(),
   image: DS.attr()
 });
